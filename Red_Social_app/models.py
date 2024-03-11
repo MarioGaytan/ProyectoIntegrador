0b5c77ae69de5a58ce1118b4bae4d3a4bca39170
unknown <arielyohaina.arrelablanco@umg.edu.mx>
2024-03-10 19:50:01 -0600

    10/03/2024 Revisión del sistema y ajustes de imagenes y modelo

--- Red_Social_app/models.py
+++ Red_Social_app/models.py
@@ -3,11 +3,11 @@ from django.db import models
 class Post(models.Model):
     id_usuario = models.IntegerField()
     usuario = models.CharField(max_length=255)
-    fecha_publicacion = models.DateField()
+    fecha_publicacion = models.DateField(auto_now_add=True)
     contenido = models.TextField()
     Imagen = models.ImageField(upload_to='uploads/', null=True, blank=True)
     
-def __str__(self):
-    return f'{self.usuario} - {self.fecha_publicacion}'
+    def __str__(self):
+        return f'{self.usuario} - {self.fecha_publicacion}'
 
 # Create your models here.
--- crud_api/urls.py
+++ crud_api/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from Red_Social_app import views
 from rest_framework import routers
 
@@ -26,3 +28,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
