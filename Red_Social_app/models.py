b234bd66e9448b93b94c2c7bc637ce33b4c8e126
Mario <marioarturo.gaytanorozco@umg.edu.mx.com>
2024-04-22 19:54:01 -0600

    22/04/2024 Actualizacion del 3er entregable del proyecto integrador

--- Red_Social_app/models.py
+++ Red_Social_app/models.py
@@ -1,11 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
+    PRIVACIDAD_OPCIONES = [
+        ('PUB', 'Público'),
+        ('CON', 'Solo contactos'),
+    ]
+    ETIQUETA_OPCIONES = [
+        ('CONV', 'Convivencia'),
+        ('ACAD', 'Académico'),
+        ('AYUD', 'Ayuda/Orientación'),
+        ('EMPL', 'Empleos'),
+    ]
+    
     id_usuario = models.IntegerField()
     usuario = models.CharField(max_length=255)
     fecha_publicacion = models.DateField(auto_now_add=True)
     contenido = models.TextField()
-    Imagen = models.ImageField(upload_to='uploads/', null=True, blank=True)
+    imagen = models.ImageField(upload_to='uploads/', null=True, blank=True)
+    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)  # Refiriéndose al modelo User
+    privacidad = models.CharField(max_length=4, choices=PRIVACIDAD_OPCIONES, default='PUB')
+    etiqueta = models.CharField(max_length=5, choices=ETIQUETA_OPCIONES, blank=True, null=True)
     
     def __str__(self):
         return f'{self.usuario} - {self.fecha_publicacion}'
